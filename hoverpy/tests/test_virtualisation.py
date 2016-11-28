07a80f7dc4a3315afb1566a4553ec02da7bcad22
Shyal <shyal@shyal.com>
2016-11-28 19:15:22 +0000

    fix test for ipv6

--- hoverpy/tests/test_virtualisation.py
+++ hoverpy/tests/test_virtualisation.py
@@ -10,17 +10,14 @@ class TestVirt(unittest.TestCase):
     r = requests.get("http://ip.jsontest.com/")
     j = r.json()
     self.assertIn('ip', j)
-    self.assertTrue(re.match('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', j['ip']))
 
   def test_playback(self):
     hoverpy = HoverPy(capture=True)
-    r = requests.get("http://ip.jsontest.com/asdf")
+    r = requests.get("http://ip.jsontest.com/foobar")
     j = r.json()
     self.assertIn('ip', j)
-    self.assertTrue(re.match('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', j['ip']))
 
     hoverpy.simulate()
-    r = requests.get("http://ip.jsontest.com/asdf")
+    r = requests.get("http://ip.jsontest.com/foobar")
     j = r.json()
     self.assertIn('ip', j)
-    self.assertTrue(re.match('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', j['ip']))
