44696fdfd797c81f605ac50b6061f434d5dd0b65
shyal <shyal@shyal.com>
2016-11-29 10:08:15 -0800

    now works with windows

--- hoverpy/config.py
+++ hoverpy/config.py
@@ -5,35 +5,45 @@ import shutil
 import urllib
 import zipfile
 import stat
+import tempfile
+from os.path import join
+from os.path import dirname
+from os.path import abspath
+from os.path import expanduser
+from os.path import isfile
 
 def getArch():
   arch = platform.machine()
   if arch == "x86_64":
     return "amd64"
-  return arch
+  return arch.lower()
 
 def getOS():
   o = platform.system().lower()
   if o == "darwin":
     return "OSX"
-  return o
+  return o.lower()
 
 dist = getOS()+"_"+getArch()
 
-dirName = os.path.dirname(os.path.abspath(__file__))
+dirName = dirname(abspath(__file__))
 
 version = '0.1.3'
 dist_version = '0.9.0'
-hoverflyDirectory = os.path.join(os.path.expanduser("~"), ".hoverfly", "bin", "dist_v"+dist_version, getOS()+"_"+getArch(),)
-hoverflyPath = os.path.join(hoverflyDirectory, "hoverfly")
+
+hoverflyDirectory = join(expanduser("~"), ".hoverfly", "bin", "dist_v"+dist_version, getOS()+"_"+getArch())
+hoverflyBinFile = "hoverfly.exe" if getOS() == "windows" else "hoverfly"
+hoverflyPath = join(hoverflyDirectory, hoverflyBinFile)
 
 def getHoverFlyBinaryPath():
-  if os.path.isfile(hoverflyPath):
+  if isfile(hoverflyPath):
     return hoverflyPath
   return ""
 
 def downloadHoverFly():
-  bundlePath = "/tmp/hoverfly_bundle_%s.zip"%dist
+  tmp = tempfile.mkdtemp()
+  bundlePath = join(tmp, "hoverfly_bundle_%s.zip"%dist)
+  hoverflyBinTempFile = join(tmp, hoverflyBinFile)
   print("DOWNLOADING HOVERFLY")
 
   url = "https://github.com/SpectoLabs/hoverfly/releases/download/v%s/hoverfly_bundle_%s.zip"%(dist_version, dist)
@@ -49,18 +59,16 @@ def downloadHoverFly():
 
   print("UNZIPPING")
   zip_ref = zipfile.ZipFile(bundlePath, 'r')
-  zip_ref.extractall("/tmp/")
+  zip_ref.extractall(tmp)
   zip_ref.close()
-  st = os.stat('/tmp/hoverfly')
-  os.chmod('/tmp/hoverfly', st.st_mode | stat.S_IEXEC)
-  print("Deleting downloaded zip file")
-  os.unlink(bundlePath)
-  return installHoverFly()
+  st = os.stat(hoverflyBinTempFile)
+  os.chmod(hoverflyBinTempFile, st.st_mode | stat.S_IEXEC)
+  return installHoverFly(hoverflyBinTempFile)
 
-def installHoverFly():
+def installHoverFly(hoverflyBinTempFile):
   if not os.path.isdir(hoverflyDirectory):
     os.makedirs(hoverflyDirectory)
-  shutil.copy("/tmp/hoverfly", hoverflyPath)
+  shutil.copy(hoverflyBinTempFile, hoverflyPath)
   if os.path.isfile(hoverflyPath):
     print("Hoverfly installed successfully: %s" % hoverflyPath)
   else:
