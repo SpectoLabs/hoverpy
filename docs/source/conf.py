8a3a2f4977a30df96734ea95a2f7df4dd5573aba
Shyal <shyal@shyal.com>
2016-12-01 14:15:09 +0000

    attempting to disable latex in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -226,8 +226,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'hoverpy.tex', u'hoverpy Documentation',
-     u'SpectoLabs', 'manual'),
+    (),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -256,8 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'hoverpy', u'hoverpy Documentation',
-     [author], 1)
+    ()
 ]
 
 # If true, show URL addresses after external links.
@@ -270,9 +268,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'hoverpy', u'hoverpy Documentation',
-     author, 'hoverpy', 'One line description of project.',
-     'Miscellaneous'),
+    (),
 ]
 
 # Documents to append as an appendix to all manuals.
