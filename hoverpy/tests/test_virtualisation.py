2c34f91ce8a77ec40f1b95252a5069edbc1a7f97
Shyal <shyal@shyal.com>
2016-12-02 14:48:45 +0000

    make the virtualisation unit test test against hoverpy's web interface, as opposed to an external url

--- hoverpy/tests/test_virtualisation.py
+++ hoverpy/tests/test_virtualisation.py
@@ -1,6 +1,5 @@
 import unittest
 import requests
-import re
 from hoverpy import HoverPy
 
 
@@ -8,20 +7,18 @@ class TestVirt(unittest.TestCase):
 
     def test_capture(self):
         hoverpy = HoverPy(capture=True, inMemory=True)
-        r = requests.get("http://ip.jsontest.com/")
-        j = r.json()
-        self.assertIn('ip', j)
+        r = requests.get("http://localhost:8888/api/v2/hoverfly")
+        self.assertIn('destination', r.json())
 
     def test_playback(self):
         hoverpy = HoverPy(capture=True, inMemory=True)
-        r = requests.get("http://ip.jsontest.com/foobar")
-        j = r.json()
-        self.assertIn('ip', j)
+        r = requests.get("http://localhost:8888/api/v2/hoverfly")
+        self.assertIn('destination', r.json())
 
         hoverpy.simulate()
-        r = requests.get("http://ip.jsontest.com/foobar")
-        j = r.json()
-        self.assertIn('ip', j)
+
+        r = requests.get("http://localhost:8888/api/v2/hoverfly")
+        self.assertIn('destination', r.json())
 
 if __name__ == '__main__':
     unittest.main()
