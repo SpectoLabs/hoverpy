b170daf068b9d45268d40fff164eaa9cd25e7c10
Shyal <shyal@shyal.com>
2016-12-06 11:04:36 +0000

    cleaned up test suite discovery script

--- hoverpy/tests/__init__.py
+++ hoverpy/tests/__init__.py
@@ -1,8 +1,7 @@
 import unittest
 
+
 def get_suite():
     "Return a unittest.TestSuite."
-    import hoverpy.tests.test_virtualisation
     loader = unittest.TestLoader()
-    suite = loader.loadTestsFromModule(hoverpy.tests.test_virtualisation)
     return loader.discover("./")
