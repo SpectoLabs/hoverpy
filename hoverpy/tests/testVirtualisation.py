58e9777b826c35b424e5d513fe45bcf651dade52
Shyal <shyal@shyal.com>
2016-12-08 15:21:58 +0000

    unit tests can now use a local python http server

--- hoverpy/tests/testVirtualisation.py
+++ hoverpy/tests/testVirtualisation.py
@@ -3,24 +3,80 @@ import unittest
 from hoverpy import HoverPy
 import hoverpy
 import logging
+from pipetestserver import ThreadedServerControl
+from pipetestserver import application
+from pipetestserver import proxiedClient
+import time
+import json
+import os
+import tempfile
 
 
 class TestVirt(unittest.TestCase):
+    endpoint = 'http://localhost:8000/'
 
     def testCapture(self):
+        server = ThreadedServerControl(application)
+        server.start()
+
         with HoverPy(capture=True):
-            r = requests.get("http://localhost:8888/api/v2/hoverfly")
-            self.assertIn('destination', r.json())
+            result = proxiedClient(2, endpoint=self.endpoint)
+            self.assertEqual(result, 4)
+
+        server.stop()
 
     def testPlayback(self):
+        server = ThreadedServerControl(application)
+        server.start()
+
         with HoverPy(capture=True) as hp:
-            r = requests.get("http://localhost:8888/api/v2/hoverfly")
-            self.assertIn('destination', r.json())
-
+            result = proxiedClient(2, endpoint=self.endpoint)
+            self.assertEqual(result, 4)
+            server.stop()
             hp.simulate()
+            result = proxiedClient(2, endpoint=self.endpoint)
+            self.assertEqual(result, 4)
 
-            r = requests.get("http://localhost:8888/api/v2/hoverfly")
-            self.assertIn('destination', r.json())
+    def testStressTest(self):
+        server = ThreadedServerControl(application)
+        server.start()
+
+        n = 10
+
+        def runChecks():
+            for i in range(n):
+                st = str(i)*2**20
+                result = proxiedClient(st, endpoint=self.endpoint)
+                self.assertEqual(result, st*2)
+
+        hoverpy.wipe()
+        start = time.time()
+        with HoverPy(capture=True):
+            runChecks()
+            print("generating data took %f" % (time.time() - start))
+
+        server.stop()
+        simulation = None
+        start = time.time()
+
+        with HoverPy() as hp:
+            open("simulation.json", "wa").write(json.dumps(hp.simulation()))
+
+        print(
+            "re-reading data, getting simulation, and writing it to disk took %f" %
+            (time.time() - start))
+
+        # for some reason, hoverfly falls over when trying to re-read the json
+        # data from disk
+        # start = time.time()
+        # with HoverPy(simulation="./simulation.json") as hp:
+        #     simulation = hp.simulation()
+        #     print(
+        #         "re-loading hoverfly, with json file, and reading it again took %f" %
+        #         (time.time() - start))
+
+        hoverpy.wipe()
+        os.unlink("simulation.json")
 
     def testSimulation(self):
         hoverpy.wipe()
@@ -28,7 +84,6 @@ class TestVirt(unittest.TestCase):
         with HoverPy(capture=True) as hp:
             r = requests.get("http://localhost:8888/api/v2/hoverfly")
             self.assertIn('destination', r.json())
-            import json
             simulation = json.dumps(hp.simulation())
         hoverpy.wipe()
         with HoverPy() as hp:
@@ -38,8 +93,6 @@ class TestVirt(unittest.TestCase):
         hoverpy.wipe()
 
     def testdbpath(self):
-        import tempfile
-        import os
         tmp = tempfile.mkdtemp()
         requestsFile = os.path.join(tmp, "requests.db")
         with HoverPy(capture=True, dbpath=requestsFile) as hp:
