d0d710b5d73dc17101b46edc7168475a650265df
Shyal <shyal@shyal.com>
2016-12-15 00:13:26 +0000

    updated

--- hoverpy/config.py
+++ hoverpy/config.py
@@ -30,7 +30,7 @@ dist = getOS()+"_"+getArch()
 
 dirName = dirname(abspath(__file__))
 
-version = '0.1.17'
+version = '0.1.20'
 dist_version = '0.9.2'
 
 hoverflyDirectory = join(
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.1.17'
+version = '0.1.20'
 
 setup(
     name='hoverpy',
