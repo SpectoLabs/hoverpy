f57c535d288d2e90e6ee12bb986c24bd8645b1c1
Tom Cunliffe <tom.cunliffe@specto.io>
2017-02-16 20:33:32 +0000

    docs refactor

---
+++ docs/conf.py
@@ -0,0 +1,95 @@
+import sys
+import os
+import shlex
+import sys
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../../'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+]
+
+templates_path = ['_templates']
+
+master_doc = 'index'
+
+project = u'HoverPy'
+copyright = u'2017, SpectoLabs'
+author = u'SpectoLabs'
+
+version = '0.1.20'
+# The full version, including alpha/beta/rc tags.
+release = version
+
+language = None
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+todo_include_todos = False
+
+if 'READTHEDOCS' not in os.environ:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
+
+html_context = {
+   'css_files': [                                                           
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
+            '_static/theme_overrides.css',   
+        ],
+    }
+
+
+html_show_sphinx = False
+
+html_show_copyright = True
+
+htmlhelp_basename = 'hoverpydoc'
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
+
+    # Latex figure (float) alignment
+    #'figure_align': 'htbp',
+}
+
+
+latex_documents = [
+    (master_doc, 'hoverpy.tex', u'HoverPy Documentation',
+     u'SpectoLabs', 'manual'),
+]
+
+man_pages = [
+    (master_doc, 'HoverPy', u'HoverPy Documentation',
+     [author], 1)
+]
+
+texinfo_documents = [
+    (master_doc, 'HoverPy', u'HoverPy Documentation',
+     author, 'HoverPy', 'Python library for Hoverfly API simulation tool',
+     'Miscellaneous'),
+]
--- docs/pages/usage/modify_soap.rst
+++ docs/pages/usage/modify_soap.rst
@@ -9,13 +9,13 @@ In this example we'll take a look at using hoverpy in conjunction with middlewar
 examples/soap/soapModify.py
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-.. literalinclude:: ../../examples/soap/soapModify.py
+.. literalinclude:: ../../../examples/soap/soapModify.py
    :language: python
    :lines: 7
 
 Above, the only real difference with ``examples/soap/soap.py`` is that we're loading up HoverPy with middleware enabled.
 
-.. literalinclude:: ../../examples/soap/soapModify.py
+.. literalinclude:: ../../../examples/soap/soapModify.py
    :language: python
    :lines: 15
 
@@ -53,31 +53,31 @@ Before jumping into the middleware, let's see what we'll be modifying.
 This is the XML that gets sent back to us after calling the ``ResolveIP`` method, as defined in http://ws.cdyne.com/ip2geo/ip2geo.asmx?WSDL. We are interested in modifying the ``City`` node.
 
 examples/soap/modify_payload.py
-~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 And here are the important parts of our payload modification script.
 
-.. literalinclude:: ../../examples/soap/modify_payload.py
+.. literalinclude:: ../../../examples/soap/modify_payload.py
    :language: python
    :lines: 7-8
 
 Above we make sure we are importing the lxml classes that will help us modify the data.
 
 
-.. literalinclude:: ../../examples/soap/modify_payload.py
+.. literalinclude:: ../../../examples/soap/modify_payload.py
    :language: python
    :lines: 19-21
 
 Let's make sure we only operate when we have a response, and it has a body. 
 
 
-.. literalinclude:: ../../examples/soap/modify_payload.py
+.. literalinclude:: ../../../examples/soap/modify_payload.py
    :language: python
    :lines: 22-29
 
 We parse our xml and turn it into an object. Remember that our program gets our IP address, then tries to geo-locate us based on our IP. The intent of our middleware is to override the city no matter what.
 
-.. literalinclude:: ../../examples/soap/modify_payload.py
+.. literalinclude:: ../../../examples/soap/modify_payload.py
    :language: python
    :lines: 31-34
 
